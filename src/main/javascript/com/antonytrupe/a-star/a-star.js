cf3a09d306c21056f4a605a27b4c15b422ab088b
antonytrupe <antony.trupe@gmail.com>
2015-09-14 00:43:55 -0400

    stable with initial test coverage

---
+++ src/main/javascript/com/antonytrupe/a-star/a-star.js
@@ -0,0 +1,349 @@
+//model must contain a method named getActions
+//getActions must return an array of Nodes
+/**
+ * @constructor
+ * @param {AI}
+ *            ai
+ * 
+ * @callback ai.getActions
+ * @return {Array.<{{state: Object, method: String,arguments:Array}}>}
+ * 
+ * 
+ * @param {ai.getActions}
+ *            ai.getActions
+ * @param {Function}
+ *            ai.gScore
+ * @param {Function}
+ *            ai.hScore
+ * @param {Function}
+ *            ai.getGoal
+ * @param {Function}
+ *            ai.setState
+ * @param {Function}
+ *            ai.getState
+ * @param options
+ */
+function AStar(ai, options) {
+	"use strict";
+	var max_depth;
+	var max_process_time;
+	var optionFunction;
+	var closed_set = {};
+	/**
+	 * @type PriorityQueue
+	 */
+	var open_set = new PriorityQueue('getF', PriorityQueue.MIN_HEAP);
+	var g_score = {};
+	var came_from = [];
+
+	// make sure the model has the right methods for a_star to use
+	if (typeof ai === "undefined") {
+		throw "no ai provided";
+	}
+	var requiredAiMembers = [ 'getActions', 'gScore', 'hScore', 'getGoal',
+			'setState', 'getState' ];
+	var missingMembers = [];
+	requiredAiMembers.forEach(function(method) {
+		if (typeof ai[method] === "undefined") {
+			missingMembers.push(method);
+		}
+	});
+	if (missingMembers.length !== 0) {
+		throw "ai missing attributes: " + JSON.stringify(missingMembers);
+	}
+
+	// now initialize some stuff
+	var start = new Node({
+		'state' : ai.getState()
+	});
+	var goal = new Node({
+		'state' : ai.getGoal()
+	});
+	// console.log('goal state');
+	// console.log(goal.getState());
+	// console.log('start state');
+	// console.log(start.getState());
+	open_set.add(start);
+	// console.log('done initializing AStar');
+
+	this.search = function() {
+		var i = 0;
+
+		while (open_set.size() > 0 && i <= 100) {
+			i++;
+			var q = open_set.peek();
+
+			// console.log('current state');
+			// console.log(q.getState());
+
+			// the stopping condition(s)
+			if (q.isEqual(goal)) {
+				// console.log('got to goal');
+				return reconstruct_path(q);
+			}
+
+			open_set.poll();
+			// console.log('update ai state');
+
+			ai.setState(q.getState());
+			// console.log(ai.getState());
+
+			// console.log('adding neighbors');
+
+			// closed_set.push(q);
+			// get actions returns an array of objects
+			ai.getActions().forEach(function(n) {
+				ai.setState(n.state);
+				var neighbor = new Node(n);
+				// console.log(neighbor._state);
+
+				neighbor.setPredecessor(q);
+				neighbor.setG(ai.gScore());
+				neighbor.setH(ai.hScore());
+
+				open_set.add(neighbor);
+			});
+
+		}
+	};
+
+	function reconstruct_path(q) {
+		// console.log('reconstruct_path');
+		// console.log(q.getState());
+		var total_path = [ q.getState() ];
+		// console.log(total_path);
+		var current = q;
+		while (current.getPredecessor()) {
+			total_path.push(current.getPredecessor().getState());
+			current = current.getPredecessor();
+		}
+
+		// console.log(total_path);
+		return total_path.reverse();
+	}
+
+}
+
+function Node(args) {
+	"use strict";
+	// f is total cost from start to goal through this node
+	this._f;
+	// h is the estimated cost from this node to the goal
+	this._h;
+	// g is the actual cost from start to this node
+	this._g;
+	//
+	this._state = args.state;
+	var predecessor = args.predecessor, _actionName = args.actionName, _args = args.args;
+
+	this.setState = function(__state) {
+		this._state = __state;
+	};
+	this.getState = function() {
+		return this._state;
+	};
+
+	// g is the actual cost from start to this node
+	this.setG = function(__g) {
+		this._g = __g;
+		this._f = this._g + this._h;
+	};
+
+	// h is the estimated cost from this node to the goal
+	this.setH = function(__h) {
+		this._h = __h;
+		this._f = this._g + this._h;
+	};
+
+	// f is total cost from start to goal through this node
+	this.getF = function() {
+		return this._f;
+	};
+
+	this.setPredecessor = function(__p) {
+		predecessor = __p;
+	};
+	this.getPredecessor = function() {
+		return predecessor;
+	};
+	this.isEqual = function(node) {
+		// console.log('Node.isEqual');
+		if (!(node instanceof Node)) {
+			return false;
+		}
+		// console.log(JSON.stringify(node.getState()));
+		// console.log(JSON.stringify(this.getState()));
+		return JSON.stringify(node.getState()) == JSON.stringify(this
+				.getState());
+	};
+}
+
+/**
+ * This is an improved Priority Queue data type implementation that can be used
+ * to sort any object type. It uses a technique called a binary heap.
+ * 
+ * For more on binary heaps see: http://en.wikipedia.org/wiki/Binary_heap
+ * 
+ * @param {String}
+ *            criteria The criteria by which to sort the objects. This should be
+ *            a property of the objects you're sorting.
+ * 
+ * @param {Number}
+ *            heapType either PriorityQueue.MAX_HEAP or PriorityQueue.MIN_HEAP.
+ */
+var PriorityQueue = function(criteria, heapType) {
+	this.length = 0; // The current length of heap.
+	var queue = [];
+	var isMax = false;
+
+	// Constructor
+	if (heapType == PriorityQueue.MAX_HEAP) {
+		isMax = true;
+	}
+
+	this.size = function() {
+		return this.length;
+	};
+
+	/**
+	 * Inserts the value into the heap and sorts it.
+	 * 
+	 * @param value
+	 *            The object to insert into the heap.
+	 * @memberOf PriorityQueue
+	 * @return
+	 */
+	this.insert = function(value) {
+		if (!value.hasOwnProperty(criteria)) {
+			throw "Cannot insert " + value
+					+ " because it does not have a property by the name of "
+					+ criteria + ".";
+		}
+		queue.push(value);
+		this.length++;
+		bubbleUp(this.length - 1);
+	};
+
+	this.add = this.insert;
+
+	/**
+	 * Peeks at the highest priority element.
+	 * 
+	 * @return the highest priority element
+	 */
+	this.getHighestPriorityElement = function() {
+		return queue[0];
+	};
+
+	this.peek = this.getHighestPriorityElement;
+
+	/**
+	 * Removes and returns the highest priority element from the queue.
+	 * 
+	 * @return the highest priority element
+	 */
+	this.shiftHighestPriorityElement = function() {
+		if (this.length === 0) {
+			throw ("There are no more elements in your priority queue.");
+		} else if (this.length === 1) {
+			var onlyValue = queue[0];
+			queue = [];
+			this.length = 0;
+			return onlyValue;
+		}
+		var oldRoot = queue[0];
+		var newRoot = queue.pop();
+		this.length--;
+		queue[0] = newRoot;
+		swapUntilQueueIsCorrect(0);
+		return oldRoot;
+	};
+
+	this.poll = this.shiftHighestPriorityElement;
+
+	/**
+	 * @memberOf PriorityQueue
+	 */
+	var bubbleUp = function(index) {
+		if (index === 0) {
+			return;
+		}
+		var parent = getParentOf(index);
+		if (evaluate(index, parent)) {
+			swap(index, parent);
+			bubbleUp(parent);
+		} else {
+			return;
+		}
+	};
+
+	var swapUntilQueueIsCorrect = function(value) {
+		var left = getLeftOf(value);
+		var right = getRightOf(value);
+		if (evaluate(left, value)) {
+			swap(value, left);
+			swapUntilQueueIsCorrect(left);
+		} else if (evaluate(right, value)) {
+			swap(value, right);
+			swapUntilQueueIsCorrect(right);
+		} else if (value == 0) {
+			return;
+		} else {
+			swapUntilQueueIsCorrect(0);
+		}
+	};
+
+	var swap = function(self, target) {
+		var placeHolder = queue[self];
+		queue[self] = queue[target];
+		queue[target] = placeHolder;
+	};
+
+	var evaluate = function(self, target) {
+		if (queue[target] === undefined || queue[self] === undefined) {
+			return false;
+		}
+
+		var selfValue;
+		var targetValue;
+
+		// Check if the criteria should be the result of a function call.
+		if (typeof queue[self][criteria] === 'function') {
+			selfValue = queue[self][criteria]();
+			targetValue = queue[target][criteria]();
+		} else {
+			selfValue = queue[self][criteria];
+			targetValue = queue[target][criteria];
+		}
+
+		if (isMax) {
+			if (selfValue > targetValue) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			if (selfValue < targetValue) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+
+	var getParentOf = function(index) {
+		return Math.floor(Math.floor((index - 1) / 2));
+	};
+
+	var getLeftOf = function(index) {
+		return index * 2 + 1;
+	};
+
+	var getRightOf = function(index) {
+		return index * 2 + 2;
+	};
+};
+
+// Constants
+PriorityQueue.MAX_HEAP = 0;
+PriorityQueue.MIN_HEAP = 1;
---
+++ src/test/javascript/com/antonytrupe/a-star/a-star.js
@@ -0,0 +1,531 @@
+QUnit.test("hello test", function(assert) {
+	assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("example PriorityQueue.MIN_HEAP", function(assert) {
+	var p = new PriorityQueue("value", PriorityQueue.MIN_HEAP);
+
+	var object1 = {};
+	var object2 = {};
+	var object3 = {};
+	var object4 = {};
+
+	object1.value = 5;
+	object2.value = 10;
+	object3.value = 20;
+	object4.value = 15;
+
+	p.insert(object1);
+	p.insert(object2);
+	p.insert(object3);
+	p.insert(object4);
+
+	assert.equal(p.peek(), object1);
+	assert.equal(p.shiftHighestPriorityElement(), object1);
+
+	assert.equal(p.peek(), object2);
+	assert.equal(p.shiftHighestPriorityElement(), object2);
+
+	assert.equal(p.peek(), object4);
+	assert.equal(p.shiftHighestPriorityElement(), object4);
+
+	assert.equal(p.peek(), object3);
+	assert.equal(p.shiftHighestPriorityElement(), object3);
+
+	assert.equal(p.length, 0);
+
+});
+
+QUnit.test("example 1 PriorityQueue.MIN_HEAP", function(assert) {
+	var p = new PriorityQueue("value", PriorityQueue.MIN_HEAP);
+
+	var object1 = {};
+	var object2 = {};
+
+	object1.value = 10;
+	object2.value = 5;
+
+	p.insert(object1);
+	assert.equal(p.peek(), object1);
+
+	p.insert(object2);
+	assert.equal(p.peek(), object2);
+
+	assert.equal(p.shiftHighestPriorityElement(), object2);
+
+});
+
+QUnit.test("reverse PriorityQueue.MIN_HEAP", function(assert) {
+	var p = new PriorityQueue("value", PriorityQueue.MIN_HEAP);
+
+	var object1 = {};
+	var object2 = {};
+	var object3 = {};
+	// var object4 = {};
+
+	object1.value = 20;
+	object2.value = 15;
+	object3.value = 10;
+	// object4.value = 5;
+
+	p.add(object1);
+	assert.equal(p.peek(), object1);
+	p.add(object2);
+	assert.equal(p.peek(), object2);
+	p.add(object3);
+	// p.insert(object4);
+
+	// assert.equal(p.peek(), object4);
+	// assert.equal(p.shiftHighestPriorityElement(), object4);
+
+	assert.equal(p.peek(), object3);
+	assert.equal(p.shiftHighestPriorityElement(), object3);
+
+	assert.equal(p.peek(), object2);
+	assert.equal(p.shiftHighestPriorityElement(), object2);
+
+	assert.equal(p.peek(), object1);
+	assert.equal(p.shiftHighestPriorityElement(), object1);
+
+	assert.equal(p.length, 0);
+
+});
+
+QUnit.test("PriorityQueue.MIN_HEAP", function(assert) {
+
+	var pq = new PriorityQueue('state', PriorityQueue.MIN_HEAP);
+	var zero = {};
+	zero.state = 10;
+
+	// console.log(zero);
+
+	var one = {};
+	one.state = 20;
+	// console.log(one);
+
+	var two = {};
+	two.state = 30;
+
+	// console.log(two);
+
+	// console.log('adding first element');
+	pq.add(two);
+	// console.log(pq);
+	assert.equal(pq.peek().state, 30, "make sure 30 is on the top");
+
+	// console.log('adding second element');
+	pq.add(one);
+	// console.log(pq);
+	// console.log(pq.peek());
+	assert.equal(pq.peek().state, 20, "make sure 20 is on the top");
+
+	// console.log('adding third element');
+	pq.add(zero);
+	// console.log(pq);
+
+	assert.equal(pq.peek().state, 10, "make sure 10 is on the top");
+});
+
+QUnit.test("deepEqual", function(assert) {
+	function State(_node) {
+		this.node = _node;
+	}
+	var one = new State(1);
+	var one1 = new State(1);
+	assert.deepEqual({
+		'node' : 1
+	}, {
+		'node' : 1
+	}, 'object type the same:object');
+
+	assert.deepEqual(one, one1, 'object type the same:State');
+	assert.notDeepEqual(one, {
+		'node' : 1
+	}, 'object type checked');
+});
+
+QUnit.test("node equality", function(assert) {
+	assert.ok(new Node({
+		'state' : 1
+	}).isEqual(new Node({
+		'state' : 1
+	})), "1==1");
+	assert.ok(!new Node({
+		'state' : 1
+	}).isEqual(new Node({
+		'state' : 2
+	})), "1==2");
+
+});
+
+QUnit.test("model validation", function(assert) {
+	assert.throws(function() {
+		new AStar();
+	}, "no ai provided", "no ai provided");
+	assert.throws(function() {
+		new AStar({});
+	}, /ai missing attributes:/, "model missing attributes");
+	assert.ok(new AStar({
+		'getActions' : function() {
+			// mock
+		},
+		'hScore' : function() {
+			// mock
+		},
+		'getStart' : function() {
+			return new Node({});
+		},
+		'getGoal' : function() {
+			// mock
+			return new Node({});
+		},
+		'gScore' : function() {
+			// mock
+		},
+		'fScore' : function() {
+			// mock
+		},
+		'setState' : function() {
+			// mock
+		},
+		'getState' : function() {
+			// mock
+			return new Node({});
+		}
+	}), "mock model");
+});
+
+QUnit.test("single node model search", function(assert) {
+	var mock_model = {
+		'getActions' : function() {
+			// mock
+			return [];
+		},
+		'hScore' : function() {
+			// mock
+		},
+
+		'getGoal' : function() {
+			// mock
+			return 0;
+		},
+		'gScore' : function() {
+			// mock
+		},
+		'fScore' : function() {
+			// mock
+		},
+		'setState' : function() {
+			// mock
+			return this;
+		},
+		'getState' : function() {
+			// mock
+			return 0;
+		}
+	};
+	assert.deepEqual(new AStar(mock_model).search(), [ 0 ],
+			"single node model search");
+});
+
+QUnit.test("2 node/one step model search", function(assert) {
+	function AI() {
+		this._state = 0;
+		this.getActions = function() {
+			// mock
+			if (this._state == 0) {
+				return [ {
+					'state' : 1
+				} ];
+			}
+			return [];
+		};
+		this.hScore = function() {
+			// mock
+		};
+
+		this.getGoal = function() {
+			// mock
+			return 1;
+		};
+		this.gScore = function() {
+			// mock
+		};
+		this.fScore = function() {
+			// mock
+		};
+
+		this.setState = function(state) {
+			// mock
+			this._state = state;
+			return this;
+		};
+		this.getState = function() {
+			// mock
+			return this._state;
+		};
+
+	}
+
+	// console.log([ 0, 1 ]);
+	// console.log(new AStar(new AI()).search());
+	assert.deepEqual(new AStar(new AI()).search(), [ 0, 1 ],
+			"2 node/one step model search");
+});
+
+QUnit.test("3 node/one step/dead-end model search", function(assert) {
+	function AI() {
+		this._state = 0;
+		this.getActions = function() {
+			// mock
+			if (this._state == 0) {
+				return [ {
+					'state' : 1
+				}, {
+					'state' : 2
+				} ];
+			}
+			return [];
+		};
+		this.hScore = function() {
+			// mock
+		};
+
+		this.getGoal = function() {
+			// mock
+			return 2;
+		};
+		this.gScore = function() {
+			// mock
+		};
+		this.fScore = function() {
+			// mock
+		};
+		this.setState = function(state) {
+			// mock
+			this._state = state;
+			return this;
+		};
+		this.getState = function() {
+			// mock
+			return this._state;
+		};
+	}
+	assert.deepEqual(new AStar(new AI()).search(), [ 0, 2 ],
+			"3 node/one step/dead-end model search");
+});
+
+QUnit.test("4 node/2 step/dead-end model search", function(assert) {
+	function State(_node) {
+		this.node = _node;
+	}
+	var zero = new State(0);
+	var one = new State(1);
+	var two = new State(2);
+	var three = new State(3);
+
+	function AI(_state) {
+		// private, use getter and setter methods
+		this._state = _state;
+
+		this.getActions = function() {
+
+			// mock
+			switch (this._state.node) {
+			case 0:
+				return [ {
+					'state' : one
+				}, {
+					'state' : two
+				} ];
+			case 2:
+
+				return [ {
+					'state' : three
+				} ];
+			}
+			return [];
+		};
+		this.hScore = function() {
+			// mock
+		};
+		this.fScore = function() {
+			// mock
+		};
+
+		this.getGoal = function() {
+			// mock
+			return three;
+		};
+		this.gScore = function() {
+			// mock
+		};
+		this.setState = function(state) {
+			// mock
+			this._state = state;
+			return this;
+		};
+		this.getState = function() {
+			return this._state;
+		};
+	}
+	// console.log([ { 'node' : 0 }, { 'node' : 2 }, { 'node' : 3 } ]);
+
+	// console.log(new AStar(new AI(zero)));
+	// return;
+	assert.deepEqual(new AStar(new AI(zero)).search(), [ zero, two, three ],
+			"4 node/2 step/dead-end model search");
+});
+
+QUnit.test("diamond:first path is shorter", function(assert) {
+
+	function State(_node) {
+		this.node = _node;
+	}
+	var zero = new State(0);
+	var one = new State(1);
+	var two = new State(2);
+	var three = new State(3);
+
+	// a mock up of thediskgame.AI
+	// the AI is responsible for creating new world/game states
+	function AI(state) {
+		this._state = state;
+		// returns an array of objects
+		// each object contains 3 attributes: the 'action' to get to the new
+		// state, the 'args' for the function in the first argument,
+		// and the 'state' resulting from calling the 'action' with the 'args'
+		this.getActions = function() {
+			// mock
+			switch (this._state.node) {
+			case 0:
+
+				return [ {
+					'state' : one
+				}, {
+					'state' : two
+				} ];
+			case 1:
+			case 2:
+				return [ {
+					'action' : '',
+					'args' : '',
+					'state' : three
+				} ];
+
+			default:
+				return [];
+			}
+		};
+		// h, heuristic, estimated cost from the node to the goal
+		this.hScore = function() {
+			switch (this._state.node) {
+			case 0:
+				return 10;
+			case 1:
+				return 4;
+			case 2:
+				return 5;
+			}
+			// mock
+		};
+		this.getGoal = function() {
+			// mock
+			return three;
+		};
+		// actual cost from start to current node
+		this.gScore = function() {
+			// mock
+		};
+		this.fScore = function(node) {
+			// mock
+		};
+		this.setState = function(_state) {
+			// mock
+			this._state = _state;
+			return this;
+		};
+		this.getState = function() {
+			return this._state;
+		};
+	}
+	assert.deepEqual(new AStar(new AI(zero)).search(), [ zero, one, three ],
+			"diamond:first path is shorter");
+});
+
+QUnit.test("diamond:second path is shorter", function(assert) {
+
+	function State(_node) {
+		this.node = _node;
+	}
+	var zero = new State(0);
+	var one = new State(1);
+	var two = new State(2);
+	var three = new State(3);
+
+	function AI(state) {
+		this._state = state;
+		this.getActions = function() {
+			// mock
+			switch (this._state.node) {
+			case 0:
+				return [ {
+					'function' : 'moveToNode',
+					'arguments' : 1,
+					'state' : one
+				}, {
+					'function' : 'moveToNode',
+					'arguments' : 2,
+					'state' : two
+				} ];
+
+				// both nodes 1 and 2 go to 3
+			case 1:
+			case 2:
+				return [ {
+					'function' : 'moveToNode',
+					'arguments' : 3,
+					'state' : three
+				} ];
+			default:
+				return [];
+			}
+		};
+		// h, heuristic, estimated cost from the node to the goal
+		this.hScore = function() {
+			// console.log(this._state.node);
+			switch (this._state.node) {
+			case 0:
+				return 10;
+			case 1:
+				return 5;
+			case 2:
+				return 4;
+			}
+			// mock
+		};
+		this.setState = function(_state) {
+			// console.log(_state);
+			// mock
+			this._state = _state;
+			// console.log(this._state);
+			return this;
+		};
+		this.getState = function() {
+			return this._state;
+		};
+		this.getGoal = function() {
+			// mock
+			return three;
+		};
+		// actual cost from start to current node
+		this.gScore = function() {
+			// mock
+			return 0;
+		};
+
+	}
+
+	assert.deepEqual(new AStar(new AI(zero)).search(), [ zero, two, three ],
+			"diamond:second path is shorter");
+});
